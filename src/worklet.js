8bf4f7ebe9830bf67a6771a166e5f8c6743103b6
Heydon <heydon@heydonworks.com>
2021-04-01 13:10:52 +0100

    squiggles

--- src/worklet.js
+++ src/worklet.js
@@ -1,26 +1,49 @@
-class HoudiniExample {
+class Scribble {
   static get inputProperties() {
     return [
-      "--houdiniExampleWidth",
-      "--houdiniExampleHeight",
-      "--houdiniExampleX",
-      "--houdiniExampleY",
-      "--houdiniExampleColor",
+      '--thickness',
+      '--amplitude',
+      '--frequency',
+      '--color'
     ];
   }
 
-  paint(ctx, size, properties) {
-    const width =
-      parseInt(properties.get("--houdiniExampleWidth")) || size.width;
-    const height =
-      parseInt(properties.get("--houdiniExampleHeight")) || size.height;
-    const x = parseInt(properties.get("--houdiniExampleX")) || 0;
-    const y = parseInt(properties.get("--houdiniExampleY")) || 0;
-    const color = String(properties.get("--houdiniExampleColor")) || "purple";
+  paint(ctx, size, props) {
+    const thickness = props.get('--thickness').value;
+    const amplitude = props.get('--amplitude').value;
+    const frequency = props.get('--frequency').value;
+    ctx.lineWidth = thickness;
+    ctx.strokeStyle = props.get('--color');
+    ctx.lineJoin = 'round';
 
-    ctx.fillStyle = color;
-    ctx.fillRect(x, y, width, height);
+    /*ctx.beginPath();
+    ctx.moveTo(0, size.height / 2);
+    ctx.lineTo(size.width, size.height / 2);
+    ctx.stroke();*/
+
+    const middle = size.height / 2;
+    const maxDist = (size.height * amplitude) / 2;
+    const floatBetween = (min, max) => {
+      min = min > max ? max : min;
+      max = max < min ? min : max;
+      return Math.random() * (max - min) + min;
+    }
+    const getY = dir => {
+      return dir === 'up' ? floatBetween(middle, middle - maxDist) : floatBetween(middle, middle + maxDist);
+    }
+
+    ctx.beginPath();
+    ctx.moveTo(0, getY('up'));
+
+    let x = 0;
+    let direction = 'down';
+    while (x < size.width) {
+      ctx.lineTo(x + frequency, getY(direction));
+      direction = direction === 'down' ? 'up' : 'down';
+      x += floatBetween(frequency / 2, frequency);
+    }
+    ctx.stroke();
   }
 }
 
-registerPaint("houdiniExample", HoudiniExample);
+registerPaint('scribble', Scribble);
